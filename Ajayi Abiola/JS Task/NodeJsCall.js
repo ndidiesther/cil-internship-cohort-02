2a723d1ad6f6ccee3b7d8c64a1d5ac83b04edfa5
Ajayi Abiola <abisberryisnow@gmail.com>
2022-01-25 14:22:48 +0100

    UX task 1

--- Ajayi Abiola/JS Task/NodeJsCall.js
+++ Ajayi Abiola/JS Task/NodeJsCall.js
@@ -8,15 +8,15 @@ const callExternalApiUsingHttp = (callback) => {
     https.get(_EXTERNAL_URL, (resp) => {
     let data = '';
     
-    // A chunk of data has been recieved.
+    
     resp.on('data', (chunk) => {
         data += chunk;
     });
     
-    // The whole response has been received. Print out the result.
+    
     resp.on('end', () => {
         return callback(data);
-       // console.log(JSON.stringify(data));
+       
     });
     
     }).on("error", (err) => {
